388765f3257e9d095039ab68b2f56f9f47109059
nicolaspenagos <nicolas.penagosm98@gmail.com>
2020-11-29 17:26:02 -0500

    Firebase database has been added to the project.

--- app/src/main/java/com/example/julimarketing_focus_group_androidnode/MainActivity.java
+++ app/src/main/java/com/example/julimarketing_focus_group_androidnode/MainActivity.java
@@ -1,11 +1,39 @@
+/**~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * @author Nicolás Penagos Montoya
+ * nicolas.penagosm98@gmail.com
+ **~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ */
+
 package com.example.julimarketing_focus_group_androidnode;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.firebase.database.FirebaseDatabase;
+
+/*
+ * This class controls the screen where the user enters their score for each question.
+ */
 public class MainActivity extends AppCompatActivity {
 
+    // -------------------------------------
+    // Database
+    // -------------------------------------
+    private FirebaseDatabase database;
+
+    // -------------------------------------
+    // XML references
+    // -------------------------------------
+    private TextView questionTextView;
+    private TextView scoreTextView;
+    private Button okButton;
+
+    // -------------------------------------
+    // Android methods
+    // -------------------------------------
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
